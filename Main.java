a3de3f6ff112ca58f5ac369c13c96fa2ca5eed9c
Luiz Vigiato <lh.vigiato@gmail.com>
2018-04-12 17:04:24 -0300

    primeiras alterações

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
+import java.io.Console;
 import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
+
+    Scanner keyboard = new Scanner(System.in);
+    Console console = System.console();
+
+    int inicio = 0;
+    String name;
     
+    do{
+      System.out.println("1 - Para Logar.");
+      System.out.println("2 - Para sair.");
+      // inicio = keyboard.nextInt();
+      if(inicio == 1){
+        name = console.readLine("Entre com o nome do usuario");
+        
+      }
+    }while(inicio != 2);
+    System.out.println("Programa finalizado");
   }
 }
