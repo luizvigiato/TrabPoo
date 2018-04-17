11082de5acd2fba7c75842127fb2e3fd262d65e8
Luiz Vigiato <lh.vigiato@gmail.com>
2018-04-17 14:26:19 -0300

    haaa

---
+++ MyEmail.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class MyEmail{
+    public EntraEmail(String nome){
+        ArrayList<Email> emails = new ArrayList<Email>();
+        emails = caixaDeEntrada(nome);
+    }
+}
--- ReadAndWrite.java
+++ ReadAndWrite.java
@@ -1,5 +1,9 @@
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 public class ReadAndWrite{
+    public ArrayList caixaDeEntrada(String nome){
+      File arq = new FileOutputStream("arq.dat");  
+    }
 }
