3bba836a7bd9b93548abe9e3987c276a036b64ab
Luiz Vigiato <lh.vigiato@gmail.com>
2018-04-21 18:32:37 -0300

    Escrever e exibir

--- ReadAndWrite.java
+++ ReadAndWrite.java
@@ -1,4 +1,6 @@
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ReadAndWrite{
     public void login(String nome, ArrayList<Email> emails){
@@ -23,11 +25,26 @@ public class ReadAndWrite{
 
         opcao = keyboard.nextInt();
         switch(opcao){
-            case 1:
-            escreverEmail();
+            case 1://gravar
+            Email novoE;
+            novoE = escreverEmail(nome);
+            emails.add(novoE);
+            System.out.println("Email enviado com sucesso");
+            // System.out.println("Deseja salvar no arquivo?\n 1-Sim\n 2-Não");
+            // int op;
+            // op = keyboard.nextInt();
+            // if(op == 1){
+            // //nao foi solicitado que carregue o arquivo
+            //     ArrayList<Email> arquivoE = new ArrayList<Email>();
+            //     arquivoE = leitura.abrirArquivo();
+            //     arquivoE.add(novoE);
+            //     gravacao.gravarTudo(arquivoE);
+            // //final do if
+            // } else {
+            // }
             break;
-            case 2:
-            listarEmails();
+            case 2://listar
+            listarEmails(nome, emails);
             break;
             case 3:
             lerEmail();
@@ -47,6 +64,25 @@ public class ReadAndWrite{
             default:
             System.out.println("Favor selecionar opcao valida");
             break;
+        }
+    }
+
+    public Email escreverEmail(String name){
+        Console console = System.console();
+
+        String output = console.readLine("Para:");
+        String titulo = console.readLine("Assunto:");
+        String mensagem = console.readLine("Mensagem:");
+
+        Email novoEmail = new Email(name,output,titulo,mensagem);
+        return novoEmail;
+    }
+
+    public void listarEmails(String name,ArrayList<Email> emails){
+        for(int i=0; i < emails.size();i++){
+            if(emails.get(i).getInput().equals(name)){//Se contiver
+                System.out.println(i + "- " + "Para: " + emaisl.get(i).getOutput() + "Assunto: " + emails.get(i).getOutput());
+            }
         }
     }
 }
