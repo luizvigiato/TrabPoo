ea158c32affd2b0b8b5973bbffd127e3bed0fcc3
Luiz Vigiato <lh.vigiato@gmail.com>
2018-04-26 16:10:06 -0300

    Trabalho Final

--- MyEmail.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class MyEmail{
-    public EntraEmail(String nome){
-        ArrayList<Email> emails = new ArrayList<Email>();
-        emails = caixaDeEntrada(nome);
-    }
-}
